e1705c1bf1b4953017e9c6d3d63cb0556e07c9b5
David Martinez <d.martinez@safened.com>
2017-11-29 23:08:21 +0100

    adddocker support

---
+++ src/api/MartenExtensions.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Marten;
+using Marten.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
+namespace API
+{
+    public static class MartenExtensions
+    {
+        public static IServiceCollection AddMArten(this IServiceCollection services, IConfiguration configuration)
+        {
+            Retry(() => services.AddSingleton<IDocumentStore>(
+                    DocumentStore.For(_ =>
+                    {
+                        _.Connection(configuration["MARTEN_DB"]);
+                        _.Events.DatabaseSchemaName = "marten_store";
+                        _.AutoCreateSchemaObjects = AutoCreate.All;
+                    })));
+
+            return services;
+        }
+
+        internal static void Retry(Action action, int retries = 5) =>
+            Policy.Handle<Exception>()
+                .WaitAndRetry(retries, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)))
+                .Execute(action);
+    }
+}
